7174bef205231ea00eb99284636a73120f211bf3
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-11-25 23:01:42 +0000

    categoriaController

--- produtos/src/main/java/com/example/produtos/Controller/CategoriaController.java
+++ produtos/src/main/java/com/example/produtos/Controller/CategoriaController.java
@@ -1,5 +1,39 @@
 package com.example.produtos.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.produtos.Model.Categoria;
+import com.example.produtos.Model.Produto;
+import com.example.produtos.Service.CategoriaService;
+import com.example.produtos.Service.ProdutosService;
+
+@RestController
+@RequestMapping("categoria")
 public class CategoriaController {
-    
+   private final CategoriaService categoriaService;
+   
+   @Autowired
+   public CategoriaController(CategoriaService categoriaService){
+    this.categoriaService = categoriaService;
+   }
+
+   @PostMapping
+   public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria){
+    Categoria novCategoria = categoriaService.criarCategoria(categoria);
+    return ResponseEntity.status(HttpStatus.CREATED).body(novaCategoria);
+   }
+
+   @GetMapping
+   public ResponseEntity<List<Categoria>> getCategoria(){
+    List<Categoria> listaDeCategoria = categoriaService.retornarCategorias();
+    return ResponseEntity.status(HttpStatus.OK).body(listaDeCategoria);
+   }
 }
